5b757a5feb54d306aca407a811e690ed820dceb9
antarikshgosain <antariksh.gosain@gmail.com>
2025-03-19 19:47:13 -0400

    gitignore file updated

--- vertx-stock-api/src/main/java/com/anta/vertx_stock_api/MainVerticle.java
+++ vertx-stock-api/src/main/java/com/anta/vertx_stock_api/MainVerticle.java
@@ -4,6 +4,7 @@ import com.sun.tools.javac.Main;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
+import io.vertx.ext.web.Router;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,12 +22,16 @@ public class MainVerticle extends AbstractVerticle {
                 LOG.error("Failed to deploy: ", ar.cause());
                 return;
             }
-            LOG.info("Deployed: ", MainVerticle.class.getName());
+            LOG.info("Deployed: {}", MainVerticle.class.getName());
         });
     }
 
     @Override
     public void start(Promise<Void> startPromise) throws Exception {
+
+        final Router restApi = Router.router(vertx);
+        restApi.get("/assets");
+
         vertx.createHttpServer().requestHandler(req -> {
             req.response()
                     .putHeader("content-type", "text/plain")
