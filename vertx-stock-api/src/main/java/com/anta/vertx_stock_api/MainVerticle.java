638707b847015ff87f2be284177cfd208ecc590b
antarikshgosain <antariksh.gosain@gmail.com>
2025-03-19 19:42:49 -0400

    custom logback xml added

--- vertx-stock-api/src/main/java/com/anta/vertx_stock_api/MainVerticle.java
+++ vertx-stock-api/src/main/java/com/anta/vertx_stock_api/MainVerticle.java
@@ -1,14 +1,28 @@
 package com.anta.vertx_stock_api;
 
+import com.sun.tools.javac.Main;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MainVerticle extends AbstractVerticle {
 
+    public static final Logger LOG = LoggerFactory.getLogger(MainVerticle.class);
+
     public static void main(String[] args) {
         var vertx = Vertx.vertx();
-        vertx.deployVerticle(new MainVerticle());
+        vertx.exceptionHandler(error -> {
+            LOG.error("Unhandled: ", error);
+        });
+        vertx.deployVerticle(new MainVerticle(), ar -> {
+            if(ar.failed()){
+                LOG.error("Failed to deploy: ", ar.cause());
+                return;
+            }
+            LOG.info("Deployed: ", MainVerticle.class.getName());
+        });
     }
 
     @Override
